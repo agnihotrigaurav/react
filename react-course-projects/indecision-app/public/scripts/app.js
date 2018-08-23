a9613d8cbc6055c0b2bc1d92d6b4eab959828f7c
Gaurav <gaurav.a.agnihotri@oracle.com>
2018-08-23 21:44:11 +0530

    test

---
+++ react-course-projects/indecision-app/public/scripts/app.js
@@ -0,0 +1,61 @@
+'use strict';
+
+console.log("App is running!");
+// JSX - JavaScript XML 
+
+var template = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Indecision App'
+    ),
+    React.createElement(
+        'p',
+        null,
+        ' This is some info'
+    ),
+    React.createElement(
+        'ol',
+        null,
+        React.createElement(
+            'li',
+            null,
+            'Item one'
+        ),
+        React.createElement(
+            'li',
+            null,
+            'Item two'
+        )
+    )
+);
+var user = {
+    name: 'Gaurav Agnihotri',
+    age: 28,
+    location: 'New Delhi'
+};
+var template1 = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        user.name
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age: ',
+        user.age
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location : ',
+        user.location
+    )
+);
+var appRoot = document.getElementById('app');
+ReactDOM.render(template1, appRoot);
