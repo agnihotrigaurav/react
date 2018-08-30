3276cf2b431e795d428d09c8a2397494b861731d
Gaurav Agnihotri <gaurav.agnihotri@rediffmail.com>
2018-08-30 22:06:47 +0530

    class based commit

--- react-course-projects/indecision-app/src/playground/build-it-visible.js
+++ react-course-projects/indecision-app/src/playground/build-it-visible.js
@@ -1,25 +1,60 @@
-console.log("App is running!");
-// JSX - JavaScript XML 
+class VisibilityToggle extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleToggleVisibility = this.handleToggleVisibility.bind(this)
+        this.state = {
+            status: false
+        }
+    }
 
-var appRoot = document.getElementById('app')
-let visibility = false;
-const toggleVisibility = () => {
-    visibility = !visibility;
-   render();
-};
+    handleToggleVisibility () {
+        this.setState( (prevState) => {
+            return {
+                status: !prevState.status
 
-const render = () => {
-    const template = (
+            };
+        });
+        console.log('handleToggleVisibility')
+    }
+    render () {
+        return (
         <div>
             <h1>Visibility Toggle</h1>
-            <button onClick={toggleVisibility}>{visibility ? 'Hide details' : 'showDetails'}</button>
-            {visibility && (
+            <button onClick={this.handleToggleVisibility}>{this.state.status ? 'Hide details' : 'showDetails'}</button>
+            {this.state.status && (
                 <div>
                     <p>Hello</p>
                 </div>
             )}
         </div>
-    );
-    ReactDOM.render(template, appRoot)
+        );
+    }
 }
-render();
+
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
+
+// console.log("App is running!");
+// // JSX - JavaScript XML 
+
+// var appRoot = document.getElementById('app')
+// let visibility = false;
+// const toggleVisibility = () => {
+//     visibility = !visibility;
+//    render();
+// };
+
+// const render = () => {
+//     const template = (
+//         <div>
+//             <h1>Visibility Toggle</h1>
+//             <button onClick={toggleVisibility}>{visibility ? 'Hide details' : 'showDetails'}</button>
+//             {visibility && (
+//                 <div>
+//                     <p>Hello</p>
+//                 </div>
+//             )}
+//         </div>
+//     );
+//     ReactDOM.render(template, appRoot)
+// }
+// render();
--- react-course-projects/indecision-app/src/playground/counter-example.js
+++ react-course-projects/indecision-app/src/playground/counter-example.js
@@ -1,47 +1,101 @@
-console.log("App is running!");
-// JSX - JavaScript XML 
 
+class Counter extends React.Component {
+    constructor (props) { 
+        super(props);
+        this.handleAddOne = this.handleAddOne.bind(this);
+        this.handleMinusOne = this.handleMinusOne.bind(this);
+        this.handleReset = this.handleReset.bind(this);
+        this.state = {
+            count: 0
+        };
+    }
 
-const user = {
-    name: 'Gaurav Agnihotri',
-    age: 28,
-    location: 'New Delhi'
-};
-const template1 = (
-    <div>
-        <h1>{user.name}</h1>
-        <p>Age: {user.age}</p>
-        <p>Location : {user.location}</p>
-    </div>
-);
-let count = 0;
-const addOne = () => {
-    count++;
-    renderCounterApp();
+    handleAddOne () {
+        this.setState( (prevState) => {
+            return {
+                count: prevState.count +1
+
+            };
+        });
+        console.log('handleAddOne');
+    }
+    handleMinusOne() {
+        this.setState( (prevState) => {
+            return {
+                count: prevState.count -1
+
+            };
+        });
+        console.log('handleMinusOne');
+    }
+
+    handleReset() {
+        this.setState( () => {
+            return {
+                count: 0
+
+            };
+        });
+        console.log('handleReset');
+    }
+    render () {
+        return (
+            <div>
+                <h1>Count: {this.state.count}</h1>
+                <button onClick={this.handleAddOne}> +1</button>
+                <button onClick={this.handleMinusOne}>-1</button>
+                <button onClick={this.handleReset}>reset</button>
+            </div>
+        );
+    }
 }
 
-const minusOne = () => {
-    count--;
-    renderCounterApp();
-}
+ReactDOM.render(<Counter />, document.getElementById('app'));
 
-const reset = () => {
-    count = 0;
-    renderCounterApp();
-}
+// console.log("App is running!");
+// // JSX - JavaScript XML 
 
-var appRoot = document.getElementById('app')
 
-const renderCounterApp = () => {
-    const template2 = (
-        <div>
-            <h1>Count: {count}</h1>
-            <button onClick={addOne}>+1</button>
-            <button onClick={minusOne}>-1</button>
-            <button onClick={reset}>RESET</button>
-        </div>
-    );
-    console.log(template2);
-    ReactDOM.render(template2, appRoot)    
-};
-renderCounterApp();
+// const user = {
+//     name: 'Gaurav Agnihotri',
+//     age: 28,
+//     location: 'New Delhi'
+// };
+// const template1 = (
+//     <div>
+//         <h1>{user.name}</h1>
+//         <p>Age: {user.age}</p>
+//         <p>Location : {user.location}</p>
+//     </div>
+// );
+// let count = 0;
+// const addOne = () => {
+//     count++;
+//     renderCounterApp();
+// }
+
+// const minusOne = () => {
+//     count--;
+//     renderCounterApp();
+// }
+
+// const reset = () => {
+//     count = 0;
+//     renderCounterApp();
+// }
+
+// var appRoot = document.getElementById('app')
+
+// const renderCounterApp = () => {
+//     const template2 = (
+//         <div>
+//             <h1>Count: {count}</h1>
+//             <button onClick={addOne}>+1</button>
+//             <button onClick={minusOne}>-1</button>
+//             <button onClick={reset}>RESET</button>
+//         </div>
+//     );
+//     console.log(template2);
+//     ReactDOM.render(template2, appRoot)    
+// };
+// renderCounterApp();
